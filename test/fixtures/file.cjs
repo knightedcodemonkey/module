f53da675ade2b5f2a7da0f7d4d058e6353394598
KCM <knightedcodemonkey@gmail.com>
2024-06-04 15:02:31 -0500

    fix: support commonjs globals as identifiers. (#4)

--- test/fixtures/file.cjs
+++ test/fixtures/file.cjs
@@ -5,9 +5,24 @@ const { esmodule } = require('./file.mjs')
 
 __filename
 __dirname
+require
 require.main
 require.cache
 require.resolve('./file.mjs')
+require.resolve(`${__dirname}/other.js`)
+
+const filename = __filename
+const detectCalledFromCli = async path => {
+  const realPath = await realpath(path)
+
+  if (__filename === pathToFileURL(realPath).href) {
+    stdout.write('invoked as cli')
+  } else {
+    throw new Error(require.resolve(filename))
+  }
+}
+
+detectCalledFromCli(argv[1])
 
 module
 module.exports
