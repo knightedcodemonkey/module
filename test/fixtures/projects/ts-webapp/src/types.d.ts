2f6428151d0f4b0040dcdb2cd648e46530cf3a2a
KCM <knightedcodemonkey@gmail.com>
2025-12-28 16:13:15 -0600

    test: fix react-jsx runtime for fixtures.

--- test/fixtures/projects/ts-webapp/src/types.d.ts
+++ test/fixtures/projects/ts-webapp/src/types.d.ts
@@ -4,3 +4,14 @@ declare namespace JSX {
     [elemName: string]: any
   }
 }
+
+declare module 'react/jsx-runtime' {
+  export const jsx: any
+  export const jsxs: any
+  export const Fragment: any
+}
+
+declare module 'react/jsx-dev-runtime' {
+  export const jsxDEV: any
+  export const Fragment: any
+}
